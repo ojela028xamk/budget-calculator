8043a7a495f91069428a9549de006b00792d7f79
ojela028xamk <jerelans91@gmail.com>
2024-01-10 13:29:57 +0200

    income expense component and nav

---
+++ src/IncomeExpenseList/IncomeExpenseList.tsx
@@ -0,0 +1,11 @@
+import { JSX } from 'react'
+
+const IncomeExpenseList = (): JSX.Element => {
+  return (
+    <div>
+      <h2>Income Expense List</h2>
+    </div>
+  )
+}
+
+export default IncomeExpenseList
